4eeec22f1cc431ad33edaad2429ddf0648c65548
florence <loracodes3@gmail.com>
2024-10-07 08:10:45 +0300

    implement function exprssion

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ saturdayFun("cook for family")
 //HOISTING 
 //JavaScript's ability to call functions before they appear in the code is called hoisting. For hoisting to work, the function must be defined using a function declaration.
 // Hoisting in JavaScript is a behavior where function and variable declarations are moved to the top of their containing scope during the compilation phase, allowing functions to be called before they are actually defined in the code.
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
