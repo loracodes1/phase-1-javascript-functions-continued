a0e0ac598b1196ee81e178618f1784b1f62edb20
florence <loracodes3@gmail.com>
2024-10-07 07:58:54 +0300

    hoisting

--- index.js
+++ index.js
@@ -8,3 +8,4 @@ saturdayFun("cook for family")
 
 //HOISTING 
 //JavaScript's ability to call functions before they appear in the code is called hoisting. For hoisting to work, the function must be defined using a function declaration.
+// Hoisting in JavaScript is a behavior where function and variable declarations are moved to the top of their containing scope during the compilation phase, allowing functions to be called before they are actually defined in the code.
