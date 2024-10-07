e965f780335295e08f3dfae5677ee6727ed3cbe5
florence <loracodes3@gmail.com>
2024-10-07 07:46:15 +0300

    define a function using function declaration

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+ function saturdayFun(activity="rollerskate"){
+    console.log(`This Saturday i want to ${activity}`)
+
+ }
+saturdayFun()
+saturdayFun("cook for family")
